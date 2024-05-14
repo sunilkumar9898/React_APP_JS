9aaffa7145e2d4a06779cee4f7f39e25868d97fc
sunil kumar <skkumarkumawatsunil1997@gmail.com>
2024-05-14 11:50:22 +0530

    updated code

---
+++ usestatecounterapp/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="counter_div">
+                <h1>{count}</h1>
+                <div className="btn">
+                    <button onClick={() => setCount(count + 1)}> add</button>
+                    <button onClick={() => setCount(0)}> Reset</button>
+                    <button  onClick={() => setCount(count - 1)} disabled={count === 0}>Reduce  </button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
