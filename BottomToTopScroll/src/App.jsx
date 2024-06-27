7fcf66f33ada162210fb505f718d2754295091b9
sunil kumar <skkumarkumawatsunil1997@gmail.com>
2024-06-27 17:55:04 +0530

    updated

---
+++ BottomToTopScroll/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+import ScrollToTop from 'react-scroll-to-top';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+          <h1>hello</h1>
+      <h1>Hello, world!</h1>
+      <h1>lorem20000 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus qui repudiandae asperiores quos libero voluptatibus possimus commodi maiores fuga. Deserunt alias est soluta eos incidunt illum, quod esse qui, nulla laudantium impedit error totam perferendis quia architecto ex necessitatibus, blanditiis ea tempore ipsum. Corporis cupiditate natus tenetur incidunt et omnis accusamus deleniti distinctio iure iusto sit, maiores, aliquam, molestias expedita libero! Saepe iste commodi quae aut nostrum nihil libero natus ex! Optio minima obcaecati, veniam deleniti cum commodi incidunt quaerat, delectus aliquam dolore excepturi nesciunt voluptates illo, similique a? Ipsum quia eum voluptates ullam impedit nisi dolor magni veritatis temporibus rem necessitatibus corrupti tenetur quod pariatur accusantium, neque quae iusto incidunt eveniet sequi facere dignissimos! Corrupti libero sapiente, fugiat, in porro quas iusto mollitia dolor corporis, earum quibusdam sed hic praesentium! Ipsum perspiciatis voluptas accusantium! Iure delectus unde qui natus maxime sapiente recusandae nemo ab tempore, odio hic tempora quasi, quod deleniti veritatis? Quam accusamus, voluptas nesciunt earum tenetur itaque velit! Perferendis aut architecto suscipit amet repellat soluta dignissimos accusamus consequuntur nulla eligendi cum illo, impedit quis, quo, minima eos aspernatur mollitia accusantium nesciunt dicta fugit culpa. Natus numquam quas possimus molestiae vel, nam eos? Architecto, illo in. Quod veniam exercitationem sapiente nam quae iusto dolor assumenda odit dolorum, obcaecati voluptatum nobis odio, mollitia aliquam enim hic eum error, maxime sit id. Quidem molestiae, ut incidunt nam blanditiis vero totam expedita dolore tempora sunt provident nesciunt impedit inventore eligendi commodi veniam, non quas omnis culpa unde quo tenetur magni vel. Ab possimus placeat a iure perspiciatis est animi optio cum voluptates error, ipsam quasi at, eius natus expedita voluptate! Repellendus odit eligendi reprehenderit nulla eos dolore, cupiditate beatae perferendis maxime magnam, quasi rerum illum iure quam, provident aspernatur sequi fugit in tenetur culpa ipsam explicabo tempore dolores. Eveniet eaque dolorum sapiente, quibusdam incidunt consequatur adipisci quia ipsum aperiam veniam in molestias! Explicabo sunt, id debitis magnam, sit dolor voluptate iure vero alias voluptas dicta natus voluptatem quaerat sed mollitia, repudiandae reprehenderit. Sint tempore a qui tempora excepturi placeat corporis ipsum consequatur, in rerum omnis voluptates, necessitatibus eveniet, laudantium accusamus et deserunt corrupti expedita. Corporis nam perspiciatis officia cumque iure ipsum dicta omnis delectus consequuntur perferendis eum beatae explicabo, magnam unde aliquam, a laudantium commodi magni, in dolores earum velit. Dolorum, obcaecati, ipsum eligendi eos voluptatibus sint nesciunt dolores, voluptates iusto quibusdam explicabo! Incidunt debitis sed doloremque voluptatibus adipisci velit aspernatur maxime iusto nemo, nobis reiciendis tenetur asperiores architecto aperiam. Tempora reprehenderit laboriosam, adipisci beatae eveniet accusantium nisi doloremque tenetur, magnam nobis aspernatur iusto voluptates eum error ratione temporibus. Blanditiis, porro. Deserunt debitis voluptatum quibusdam, deleniti excepturi, accusantium incidunt ipsam maxime esse magnam optio necessitatibus in fugiat. In, soluta! Sequi magni natus itaque? Atque vitae, totam quae architecto excepturi aliquam quam sapiente maxime voluptate laboriosam labore, dolorem minus nam quidem impedit porro molestiae non voluptatibus natus corrupti ipsa laudantium quo! Beatae assumenda et mollitia, deleniti minus molestias aliquam. Nesciunt, repudiandae vel. Sed voluptate neque minus corporis magni, consequuntur amet aliquid.</h1>
+          <div style={{ marginTop: "10vh" }} />
+      <ScrollToTop
+        style={{backfaceVisibility:"red"}}
+              smooth
+              viewBox="0 0 24 24"
+              svgPath="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21"
+          />
+      </>
+  );
+}
+
+export default App
